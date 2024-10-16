34915e23b272b9a18a55b0efcf305239924204ed
MiguelMO2311 <mmeneses73@gmail.com>
2024-10-16 20:37:23 +0200

    subida de los scripts

---
+++ despedida.py
@@ -0,0 +1,9 @@
+
+def despedida():
+    """
+    Imprime una despedida en la consola.
+    """
+    print("Adiós desde script2")
+
+if __name__ == "__main__":
+    despedida()
---
+++ saludo.py
@@ -0,0 +1,9 @@
+
+def saludo():
+    """
+    Imprime un saludo en la consola.
+    """
+    print("Hola desde script1")
+
+if __name__ == "__main__":
+    saludo()
